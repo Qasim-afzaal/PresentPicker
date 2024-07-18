073d55dbfcc8bf88ca416056ed36dd53bfab9266
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-19 00:38:13 +0500

    Handle App colors

---
+++ lib/styles/app_color.dart
@@ -0,0 +1,19 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  AppColors._();
+
+  static HexColor primary = HexColor("#0018EE");
+  static HexColor secondary = HexColor("#76F016");
+  static HexColor lightSecondary = HexColor("#DDFFC2");
+
+  static HexColor splashBackground = HexColor("#FDFDFD");
+  static HexColor homeBackground = HexColor("#FFFFFF");
+
+  static HexColor black = HexColor("#000000");
+  static HexColor white = HexColor("#ffffff");
+
+  static HexColor danger = HexColor("#FF4848");
+  static HexColor warning = HexColor("#3798CE");
+  static HexColor disable = HexColor("#E0E0E0");
+}
