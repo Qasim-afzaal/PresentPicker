39ec41e0895748fb23b064f8627ee4db5ddadede
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-23 23:53:53 +0500

    handle natively external sharing

--- android/app/src/main/kotlin/com/example/present_picker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/present_picker/MainActivity.kt
@@ -1,5 +1,42 @@
-package com.example.present_picker
+package com.example.the_gift_guide
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private var sharedData: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleDeepLink(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleDeepLink(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handleSharedData()
+    }
+
+    private fun handleDeepLink(intent: Intent) {
+        if (Intent.ACTION_VIEW == intent.action) {
+            val uri: Uri? = intent.data
+            sharedData = uri?.toString()
+            handleSharedData()
+        }
+    }
+
+    private fun handleSharedData() {
+        sharedData?.let { data ->
+            // Now you can send the sharedData to Flutter using a MethodChannel
+            // or you can directly display it in your Flutter UI
+            sharedData = null // Reset sharedData after handling
+        }
+    }
+}
