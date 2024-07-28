5c7aa4b785dca0c44e199c8aa77f81291234254f
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-28 21:53:04 +0500

    add wishist screen

---
+++ lib/views/wishlist/wish_list_view.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+//
+// class WishListView extends StatefulWidget {
+//   static const String id = "/wish-list-view";
+//   const WishListView({Key? key}) : super(key: key);
+//
+//   @override
+//   State<WishListView> createState() => _WishListViewState();
+// }
+//
+// class _WishListViewState extends State<WishListView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return WillPopScope(
+//       onWillPop: () async {
+//         return false;
+//       },
+//       child: const Scaffold(
+//         body: Center(
+//           child: Text("Wish List Screen"),
+//         ),
+//       ),
+//     );
+//   }
+// }
