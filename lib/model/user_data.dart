99a111b2c35fc9c5ee74a35d074af801194c4637
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-28 21:53:55 +0500

    add user data

---
+++ lib/model/user_data.dart
@@ -0,0 +1,24 @@
+// import 'package:json_annotation/json_annotation.dart';
+
+// part 'user_data.g.dart';
+
+// @JsonSerializable()
+// class UserData {
+//   String?complete_address;
+//   String? end_point;
+//   String? host;
+//   String? port;
+//   String? protocol;
+
+//   UserData(
+//       {this.complete_address,
+//       this.end_point,
+//       this.host,
+//       this.port,
+//       this.protocol});
+
+//   factory UserData.fromJson(Map<String, dynamic> map) =>
+//       _$UserDataFromJson(map);
+
+//   Map<String, dynamic> toJson() => _$UserDataToJson(this);
+// }
